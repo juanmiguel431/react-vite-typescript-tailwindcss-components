5278804554bb5ab160d0d725d0117245cd188f8a
juanmiguel431 <juanmiguel431@gmail.com>
2023-10-28 20:56:35 -0400

    classnames library implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,28 @@ function App() {
       </button>
       {counter}
       <Button primary>Click Me!</Button>
+      <Button secondary>Secondary!</Button>
       <Button success>Buy Now!</Button>
       <Button warning>See Deal!</Button>
       <Button danger>Hide Ads!</Button>
-      <Button outline>Something!</Button>
+      <hr/>
+      <Button primary rounded>Click Me!</Button>
+      <Button secondary rounded>Secondary!</Button>
+      <Button success rounded>Buy Now!</Button>
+      <Button warning rounded>See Deal!</Button>
+      <Button danger rounded>Hide Ads!</Button>
+      <hr/>
+      <Button primary outline>Click Me!</Button>
+      <Button secondary outline>Secondary!</Button>
+      <Button success outline>Buy Now!</Button>
+      <Button warning outline>See Deal!</Button>
+      <Button danger outline>Hide Ads!</Button>
+      <hr/>
+      <Button primary outline rounded>Click Me!</Button>
+      <Button secondary outline rounded>Secondary!</Button>
+      <Button success outline rounded>Buy Now!</Button>
+      <Button warning outline rounded>See Deal!</Button>
+      <Button danger outline rounded>Hide Ads!</Button>
     </div>
   )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import React, { PropsWithChildren } from 'react';
 import PropTypes from 'prop-types';
+import className from 'classnames';
 
 type ButtonProps = PropsWithChildren & {
   rounded?: boolean;
@@ -13,10 +14,28 @@ type ButtonProps = PropsWithChildren & {
   | { primary?: never; secondary?: never; success?: never; warning?: never; danger?: true; }
   );
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
+const Button: React.FC<ButtonProps> = (
+  { children, primary, secondary, success, warning, danger,
+    rounded, outline }) => {
+
+  const classes = className('px-3 py-1.5 border block', {
+    'border-blue-600 bg-blue-500 text-white': primary,
+    'border-gray-900 bg-gray-800 text-white': secondary,
+    'border-green-600 bg-green-500 text-white': success,
+    'border-yellow-600 bg-yellow-500 text-white': warning,
+    'border-red-600 bg-red-500 text-white': danger,
+    'rounded-full': rounded,
+    'bg-white': outline,
+    'text-blue-500': outline && primary,
+    'text-gray-900': outline && secondary,
+    'text-green-500': outline && success,
+    'text-yellow-400': outline && warning,
+    'text-red-500': outline && danger
+  })
 
   return (
-    <button className="bg-red-500">
+    <button
+      className={classes}>
       {children}
     </button>
   );
