04ee01cad43851121939df954738db9a269cda22
juanmiguel431 <juanmiguel431@gmail.com>
2023-10-29 13:01:07 -0400

    ButtonPage fixed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,8 @@
-import { useState } from 'react';
-import Button from './components/Button.tsx';
-import { GoBell, GoDownload, GoDatabase } from 'react-icons/go';
+import ButtonPage from './pages/ButtonPage.tsx';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
   return (
-    <div>
-      <button
-        onClick={() => {
-          setCounter((prev) => prev + 1);
-        }}>Counter
-      </button>
-      {counter}
-      <Button>Plain</Button>
-      <Button primary onClick={() => alert('Hello there')} className="mb-5">
-          <GoBell />
-          Click Me!
-      </Button>
-      <Button secondary>
-          <GoDownload />
-          Secondary!
-      </Button>
-      <Button success>
-          <GoDatabase />
-          Buy Now!
-      </Button>
-      <Button warning>See Deal!</Button>
-      <Button danger>Hide Ads!</Button>
-      <hr/>
-      <Button rounded>Plain</Button>
-      <Button primary rounded>
-          <GoBell />
-          Click Me!
-      </Button>
-      <Button secondary rounded>
-          <GoDownload />
-          Secondary!
-      </Button>
-      <Button success rounded>
-          <GoDatabase />
-          Buy Now!
-      </Button>
-      <Button warning rounded>See Deal!</Button>
-      <Button danger rounded>Hide Ads!</Button>
-      <hr/>
-      <Button outline>Plain</Button>
-      <Button primary outline>
-          <GoBell />
-          Click Me!
-      </Button>
-      <Button secondary outline>
-          <GoDownload />
-          Secondary!
-      </Button>
-      <Button success outline>
-          <GoDatabase />
-          Buy Now!
-      </Button>
-      <Button warning outline>See Deal!</Button>
-      <Button danger outline>Hide Ads!</Button>
-      <hr/>
-      <Button outline rounded>Plain</Button>
-      <Button primary outline rounded>Click Me!</Button>
-      <Button secondary outline rounded>Secondary!</Button>
-      <Button success outline rounded>Buy Now!</Button>
-      <Button warning outline rounded>See Deal!</Button>
-      <Button danger outline rounded>Hide Ads!</Button>
-    </div>
+    <ButtonPage />
   )
 }
 
--- src/pages/ButtonPage.tsx
+++ src/pages/ButtonPage.tsx
@@ -1,8 +1,73 @@
+import Button from '../components/Button.tsx';
+import { GoBell, GoDatabase, GoDownload } from 'react-icons/go';
+import { useState } from 'react';
 
 const ButtonPage = () => {
+  const [counter, setCounter] = useState(0);
 
   return (
-    <ButtonPage />
+    <div>
+      <button
+        onClick={() => {
+          setCounter((prev) => prev + 1);
+        }}>Counter
+      </button>
+      {counter}
+      <Button>Plain</Button>
+      <Button primary onClick={() => alert('Hello there')} className="mb-5">
+        <GoBell />
+        Click Me!
+      </Button>
+      <Button secondary>
+        <GoDownload />
+        Secondary!
+      </Button>
+      <Button success>
+        <GoDatabase />
+        Buy Now!
+      </Button>
+      <Button warning>See Deal!</Button>
+      <Button danger>Hide Ads!</Button>
+      <hr/>
+      <Button rounded>Plain</Button>
+      <Button primary rounded>
+        <GoBell />
+        Click Me!
+      </Button>
+      <Button secondary rounded>
+        <GoDownload />
+        Secondary!
+      </Button>
+      <Button success rounded>
+        <GoDatabase />
+        Buy Now!
+      </Button>
+      <Button warning rounded>See Deal!</Button>
+      <Button danger rounded>Hide Ads!</Button>
+      <hr/>
+      <Button outline>Plain</Button>
+      <Button primary outline>
+        <GoBell />
+        Click Me!
+      </Button>
+      <Button secondary outline>
+        <GoDownload />
+        Secondary!
+      </Button>
+      <Button success outline>
+        <GoDatabase />
+        Buy Now!
+      </Button>
+      <Button warning outline>See Deal!</Button>
+      <Button danger outline>Hide Ads!</Button>
+      <hr/>
+      <Button outline rounded>Plain</Button>
+      <Button primary outline rounded>Click Me!</Button>
+      <Button secondary outline rounded>Secondary!</Button>
+      <Button success outline rounded>Buy Now!</Button>
+      <Button warning outline rounded>See Deal!</Button>
+      <Button danger outline rounded>Hide Ads!</Button>
+    </div>
   );
 };
 
