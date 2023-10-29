099de8eb991853f932526646833172d878b9ee38
juanmiguel431 <juanmiguel431@gmail.com>
2023-10-28 21:50:45 -0400

    Margin bottom added to one button.

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       </button>
       {counter}
       <Button>Plain</Button>
-      <Button primary onClick={() => alert('Hello there')}>
+      <Button primary onClick={() => alert('Hello there')} className="mb-5">
           <GoBell />
           Click Me!
       </Button>
