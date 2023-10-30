7d20728b591d78687519a4d3c1c45da42b620b0c
juanmiguel431 <juanmiguel431@gmail.com>
2023-10-29 20:08:46 -0400

    Panel - className refactored.

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -3,8 +3,8 @@ import classNames from 'classnames';
 
 type PanelProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
-const Panel: React.FC<PanelProps> = ({ children, ...rest}) => {
-  const classes = classNames('border rounded p-3 shadow bg-white w-full', rest.className);
+const Panel: React.FC<PanelProps> = ({ children, className, ...rest}) => {
+  const classes = classNames('border rounded p-3 shadow bg-white w-full', className);
   return (
     <div {...rest} className={classes} >
       {children}
