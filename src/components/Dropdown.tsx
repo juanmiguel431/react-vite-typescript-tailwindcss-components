5cd299ecd7e4499ca9f0f5e02bff588897733260
juanmiguel431 <juanmiguel431@gmail.com>
2023-10-29 20:01:18 -0400

    Dropdown - styled.

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Option } from '../models';
+import { GoChevronDown } from 'react-icons/go'
 
 type DropdownProps = {
   options: Option[];
@@ -34,21 +35,29 @@ const Dropdown: React.FC<DropdownProps> = ({ options, value, onChange }) => {
 
   return (
     <div
+      className="w-48 relative"
       ref={dropDrownRef}
       onClick={() => {
         setExpanded(prev => !prev);
       }}>
-      <div>
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full">
         {value?.label ?? 'Select...'}
+        <GoChevronDown className="text-xl"/>
       </div>
-      {expanded && options.map(option => {
-        return (
-          <div
-            key={option.value}
-            onClick={() => onChange(option)}
-          >{option.label}</div>
-        );
-      })}
+      {expanded &&
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {options.map(option => {
+            return (
+              <div
+                className="hover:bg-sky-100 rounded cursor-pointer p-1"
+                key={option.value}
+                onClick={() => onChange(option)}
+              >{option.label}</div>
+            );
+          })}
+        </div>
+      }
     </div>
   );
 };
